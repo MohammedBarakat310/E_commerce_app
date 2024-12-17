c76859c4b9cafa9b9b8e97f5e41140857a6cf61d
mohammed <mb3461813@gmail.com>
2024-12-17 19:49:06 +0200

    initial commit

---
+++ ECommerceApplication/app/src/main/java/com/example/e_commerceapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_commerceapplication;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/e_commerceapplication/ui/activities/TermsActivity.java
@@ -0,0 +1,18 @@
+package com.example.e_commerceapplication.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.e_commerceapplication.R;
+
+
+public class TermsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
